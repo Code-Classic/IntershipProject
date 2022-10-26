f80ba0f787d9191a5b0de4266967d369a6067eba
Code-Classic <110042627+Code-Classic@users.noreply.github.com>
2022-10-26 23:32:03 +0530

    Add all my files

---
+++ insurancems/src/main/java/com/code/insurancems/InsurancemsApplication.java
@@ -0,0 +1,13 @@
+package com.code.insurancems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancemsApplication.class, args);
+	}
+
+}
